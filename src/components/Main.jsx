43bb1bfa5893668b8b39e2865ceb8b5bedd29571
Jean Loaiza <jc.loaiza89@gmail.com>
2022-04-23 23:27:52 -0500

    Configuración de firebase

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,7 +15,6 @@ const Main = () => {
     const [modoEdicion, setModoEdicion] = React.useState(false)
     const [error, setError] = React.useState(null)
 
-    
     React.useEffect(() => {
         const obtenerDatos = async () => {
             try {
